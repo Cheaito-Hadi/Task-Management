db07834c7b13ab6d1fe816a942e10262dfbc2d48
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-12-20 16:07:20 +0200

    Server <> Creating Database

--- backend/database/migrations/2023_12_20_125845_user_types.php
+++ backend/database/migrations/2023_12_20_125845_user_types.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_types', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->timestamps();
+            $table->text('extra');
         });
+        Schema::enableForeignKeyConstraints();
     }
 
     /**
@@ -25,6 +24,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        {
+            Schema::dropIfExists('user_types');
+        }
     }
 };
