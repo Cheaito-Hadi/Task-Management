5fded44e314f34b5ed297e0efb52b46bf5efa10c
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-12-21 21:36:47 +0200

    Client <> Creating Input Component

---
+++ frontend/src/pages/Login/index.jsx
@@ -0,0 +1,13 @@
+import LoginForm from '../../components/ui/AuthenticationForms/LoginForm'
+import React from 'react';
+import "./styles.css";
+
+const Login = () => {
+    return (
+        <div>
+           <LoginForm/>
+        </div>
+    );
+};
+
+export default Login;
