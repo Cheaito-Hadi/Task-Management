bc13df5c4e3d7b254b856725b21121b456e9d7c1
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-12-22 19:29:50 +0200

    Client <> Structuring Home Page

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import "./styles.css";
+
 const Home = () => {
     return (
-        <div className="testtt">This is home</div>
+        <div className="home-container">
+            <div className="create-task-container">
+                Add Task:
+                <div></div>
+            </div>
+            <div className="view-task-container">
+                View Tasks:
+                <div>
+                    <div>Card</div>
+                </div>
+
+            </div>
+        </div>
     );
 };
 
