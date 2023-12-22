1630e8777f8a494f1caf2b8134276b40626e9493
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-12-23 01:15:48 +0200

    Client <> Add Task

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -13,8 +13,8 @@ class TaskController extends Controller
             'title' => $request->title,
             'description' => $request->description,
             'due_date' => $request->due_date,
-            'status' => $request->status,
-            'user_id' => auth()->id(), 
+            'status' => "not finished",
+            'user_id' => auth()->id(),
         ]);
 
         return response()->json([
@@ -30,7 +30,7 @@ class TaskController extends Controller
         if (is_null($deletedTask)) {
             return response()->json(["message" => 'Task not found']);
         }
-        
+
         $deletedTask->delete();
 
         return response()->json(['message' => 'Task deleted successfully']);
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,19 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./styles.css";
+import axios from "axios";
 
 const Home = () => {
+
+    const [newTask, setNewTask] = useState({
+        title: "",
+        description: "",
+        due_date: "",
+    });
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setNewTask({...newTask, [name]: value});
+    };
+
+    const handleAddTask = async () => {
+        debugger
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/api/addTask', newTask, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`
+                },
+            });
+            console.log('Task added successfully:', response.data);
+            setNewTask({
+                title: "",
+                description: "",
+                due_date: "",
+            });
+        } catch (error) {
+            console.error('Error adding task:', error);
+        }
+    };
+
     return (
         <div className="home-container">
+            <h2 className="task-title">Add Task:</h2>
             <div className="create-task-container">
-                Add Task:
-                <div></div>
+                <div>
+                    <label className="task-title">Title:</label>
+                    <input
+                        type="text"
+                        name="title"
+                        value={newTask.title}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <label className="task-title">Description:</label>
+                <textarea
+                    name="description"
+                    value={newTask.description}
+                    onChange={handleInputChange}
+                />
+
+                <label>Due Date:</label>
+                <input
+                    type="datetime-local"
+                    name="due_date"
+                    value={newTask.due_date}
+                    onChange={handleInputChange}
+                />
+                <button onClick={handleAddTask}>Add Task</button>
             </div>
+
             <div className="view-task-container">
                 View Tasks:
                 <div>
                     <div>Card</div>
                 </div>
-
             </div>
         </div>
     );
