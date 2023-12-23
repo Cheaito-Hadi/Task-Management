11410be11e81d19274ba2e783331be01cd3ae0f5
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-12-24 01:12:47 +0200

    Server <> Adding User Types roles

--- backend/database/migrations/2023_12_20_125845_user_types.php
+++ backend/database/migrations/2023_12_20_125845_user_types.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -16,6 +17,10 @@ return new class extends Migration
             $table->string('name');
             $table->text('extra');
         });
+        DB::table('user_types')->insert([
+            ['id' => 1, 'name' => 'Employer', 'extra' => ''],
+            ['id' => 2, 'name' => 'Employee', 'extra' => ''],
+        ]);
         Schema::enableForeignKeyConstraints();
     }
 
