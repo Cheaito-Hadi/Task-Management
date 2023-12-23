8b6405684dfccebff2fa9232ff2ea4cb3de94c32
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-12-23 20:36:45 +0200

    Client <> Refactoring Task Card

--- frontend/src/components/ui/TaskCard/index.jsx
+++ frontend/src/components/ui/TaskCard/index.jsx
@@ -1,24 +1,37 @@
-import React from 'react'
-import './styles.css'
+import React from 'react';
+import './styles.css';
 
-function TaskCard({title, description, due_date, status}) {
+function TaskCard({title, description, due_date, status, onDelete, onEdit}) {
+    const handleDelete = () => {
+        onDelete();
+    };
+
+    const handleEdit = () => {
+        onEdit();
+    };
 
     return (
         <div className="card-container">
             <div className="card-wrapper">
-                <div>{title}</div>
+                <div className="title-container">
+                    <div className="title">{title}</div>
+                    <div className="btns-wrapper">
+                        <button className="edit-button" onClick={handleEdit}>
+                            Edit
+                        </button>
+                        <button className="delete-button" onClick={handleDelete}>
+                            Delete
+                        </button>
+                    </div>
+                </div>
                 <div className="status-switch-wrapper">
                     <div className="status">Status: {status}</div>
-                    <label className="switch">
-                        <input type="checkbox"/>
-                        <span className="slider round"></span>
-                    </label>
                 </div>
                 <div className="description">{description}</div>
                 <div className="due-date">Due Date: {due_date}</div>
             </div>
         </div>
-    )
+    );
 }
 
 export default TaskCard;
